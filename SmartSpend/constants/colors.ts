5179d39ca53182a4d7434c7c25f3bce1969189ff
iamskotanni <133511028+iamskotanni@users.noreply.github.com>
2025-02-03 12:48:30 +0200

    Create colors.ts

---
+++ SmartSpend/constants/colors.ts
@@ -0,0 +1,21 @@
+const tintColorLight = '#4560CF';
+const tintColorDark = '#fff';
+export const primaryColor = "#2A2D57"
+
+
+export default {
+  light: {
+    text: '#000',
+    background: '#fff',
+    tint: tintColorLight,
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: '#fff',
+    background: '#000',
+    tint: tintColorDark,
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColorDark,
+  },
+};
