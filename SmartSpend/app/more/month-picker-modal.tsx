b00d2dcc815d144755abead8453cc6065020bffe
iamskotanni <133511028+iamskotanni@users.noreply.github.com>
2025-02-03 12:19:58 +0200

    Create month-picker-modal.tsx

---
+++ SmartSpend/app/more/month-picker-modal.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SelectMonthModal = () => {
+  return (
+    <View>
+      <Text>SelectMonthModal</Text>
+    </View>
+  )
+}
+
+export default SelectMonthModal
